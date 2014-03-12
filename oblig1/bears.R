3cc72402a02e6c96612e081300b52082ee081da4
Marius Berge Eide <mariusbe@gmail.com>
2014-03-13 00:07:26 +0100

    Final, estimates R2, adj R2 and cross validated R2

--- oblig1/bears.R
+++ oblig1/bears.R
@@ -9,21 +9,28 @@ weight = bears$WEIGHT
 length = bears$LENGTH
 chest  = bears$CHEST
 
+par(ps = 22, cex = 2, cex.main = 2)
+
 # Boxplot: weight, length, chest:
 pdf('a1-weight_length_chest.pdf')
 par(mfrow=c(1,3))
-boxplot(weight, ylab="Weight [pounds]")
-boxplot(length, ylab="Length [inches]")
-boxplot(chest, ylab="Chest circumference [inches]")
+boxplot(weight, ylab="Weight [pounds]",
+        cex=3, cex.main=1.8,cex.lab=1.8,cex.axis=1.8)
+boxplot(length, ylab="Length [inches]",
+        cex=3, cex.main=1.8,cex.lab=1.8,cex.axis=1.8)
+boxplot(chest, ylab="Chest circumference [inches]",
+        cex=3, cex.main=1.8,cex.lab=1.8,cex.axis=1.8)
 dev.off()
 
 # Scatterplot: length vs weight and chest vs weight
 pdf('a2-weight_length.pdf')
-plot(weight, length, xlab="Weight [lbs]", ylab="Length [in]")
+plot(weight, length, xlab="Weight [lbs]", ylab="Length [in]",
+     col="red", pch=19, cex=3, cex.main=2,cex.lab=1.8,cex.axis=1.8)
 dev.off()
 
 pdf('a3-weight_chest.pdf')
-plot(weight, chest, xlab="Weight [lbs]", ylab="Chest circumference [in]")
+plot(weight, chest, xlab="Weight [lbs]", ylab="Chest circumference [in]",
+     col="red", pch=19, cex=3, cex.main=2,cex.lab=1.8,cex.axis=1.8)
 dev.off()
 
 # Are there any correlations?
@@ -43,10 +50,12 @@ weightmod_2 = lm(log(weight)~log(length)+log(chest))
 ### C) PLOTS OF RESIDUALS
 
 pdf('c1-weightmod_1-residuals.pdf')
-plot(weightmod_1, 1)
+plot(weightmod_1, 1,
+     col="blue", pch=19, cex=3, cex.main=2,cex.lab=1.8,cex.axis=1.8)
 dev.off()
 pdf('c2-weightmod_2-residuals.pdf')
-plot(weightmod_2, 1)
+plot(weightmod_2, 1,
+     col="blue", pch=19, cex=3, cex.main=2,cex.lab=1.8,cex.axis=1.8)
 dev.off()
 
 
@@ -74,9 +83,79 @@ bearsOld$SEX    = factor(bearsOld$SEX)
 bearsOld$MONTH  = factor(bearsOld$MONTH)
 
 weightmod_5 = lm(log(WEIGHT)~log(LENGTH)+log(CHEST)+log(NECK)+log(HEADWTH)+
-                 log(HEADLEN)+SEX+MONTH+log(AGE), data=bearsOld)
+                 log(HEADLEN)+SEX+MONTH+log(AGE), data=bearsOld,
+                 x=T, y=T)
 sink("g_output.txt")
 print(summary(weightmod_5))
 sink()
 
+
+### H) CROSS-VALIDATED R SQUARED
+
+cv.R2=function(lmobj,y=lmobj$y,x=lmobj$x)
+{
+   a=t(x)%*%x
+   d=diag(1/eigen(a)$values)
+   e=eigen(a)$vector
+   inv.a=e %*% d %*% t(e)
+   v=diag(x%*%inv.a%*%t(x))
+   SSkryss=sum((lmobj$res/(1-v))^2)
+   SStot=sum((y-mean(y))^2)
+   R2kryss=1-SSkryss/SStot
+   R2kryss
+}
+
+print("Cross-validated R2 for huge dataset:")
+print(cv.R2(weightmod_5))
+
+
+### I) FORWARD SELECTION
+
+sumry = function(arg){
+    lm(arg, data=bearsOld, x=T,y=T)
+}
+
+model = lapply(c(
+        log(WEIGHT)~log(CHEST),
+        log(WEIGHT)~log(CHEST)+log(LENGTH),
+        log(WEIGHT)~log(CHEST)+log(LENGTH)+log(HEADWTH),
+        log(WEIGHT)~log(CHEST)+log(LENGTH)+log(HEADWTH)+log(AGE),
+        log(WEIGHT)~log(CHEST)+log(LENGTH)+log(HEADWTH)+log(AGE)+MONTH,
+        log(WEIGHT)~log(CHEST)+log(LENGTH)+log(HEADWTH)+log(AGE)+MONTH+
+        log(HEADLEN),
+        log(WEIGHT)~log(CHEST)+log(LENGTH)+log(HEADWTH)+log(AGE)+MONTH+
+        log(HEADLEN)+log(NECK),
+        log(WEIGHT)~log(CHEST)+log(LENGTH)+log(HEADWTH)+log(AGE)+MONTH+
+        log(HEADLEN)+log(NECK)+SEX
+                 ), sumry)
+
+model.cvR2s = NULL 
+model.R2s   = NULL 
+model.adjR2s= NULL 
+
+for (i in 1:length(model)) {
+    model.cvR2s[i]  = cv.R2(model[[i]])
+    model.R2s[i]    = summary(model[[i]])$r.squared
+    model.adjR2s[i] = summary(model[[i]])$adj.r.squared
+}
+
+
+pdf("i-model_crossvalR2.pdf")
+plot(model.cvR2s,
+     xlab="Model number", ylab="R2",
+     ylim=c(0.93,0.99),
+     type='o', col="red", pch=19, lty=1,
+     cex=3, cex.main=2,cex.lab=1.8,cex.axis=1.8)
+lines(model.R2s, type='o', col='blue', lty=2, pch=12,
+     cex=3, cex.main=2,cex.lab=1.8,cex.axis=1.8)
+lines(model.adjR2s, type='o', lty=4,
+     cex=3, cex.main=2,cex.lab=1.8,cex.axis=1.8)
+dev.off()
+
+print("R squared")
+print(model.R2s)
+print("Adj. R squared")
+print(model.adjR2s)
+print("Cross validated R2")
+print(model.cvR2s)
 
