d8620c5a9969b25e8ba4711e938686342d59e3a0
Marius Berge Eide <mariusbe@gmail.com>
2014-03-12 21:16:41 +0100

    Done with g

--- oblig1/bears.R
+++ oblig1/bears.R
@@ -36,5 +36,47 @@ print(cor(weight, chest, method="spearman"))
 
 ### B) FIT LINEAR MODEL
 
-weightmod = lm(weight~length+chest)
+weightmod_1 = lm(weight~length+chest)
+weightmod_2 = lm(log(weight)~log(length)+log(chest))
+
+
+### C) PLOTS OF RESIDUALS
+
+pdf('c1-weightmod_1-residuals.pdf')
+plot(weightmod_1, 1)
+dev.off()
+pdf('c2-weightmod_2-residuals.pdf')
+plot(weightmod_2, 1)
+dev.off()
+
+
+### D) REMOVE OUTLIER no 52
+
+bearsImp = bears[-52,]
+weightmod_3 = lm(log(WEIGHT)~log(LENGTH)+log(CHEST), data=bearsImp)
+print(summary(weightmod_3))
+
+
+### E) USE ONLY OLD BEARS
+bearsOld = bearsImp[bearsImp$AGE>12,]
+weightmod_4 = lm(log(WEIGHT)~log(LENGTH)+log(CHEST), data=bearsOld)
+print(summary(weightmod_4))
+
+
+### F) FIND EXPECTED WEIGHT FOR 65 in long, 40 in round BEAR
+print("Expected weight: ex f):")
+print(exp(-6.061)*65^(.9934)*40^(1.983))
+
+
+### G) USE ALL COVARIATES
+
+bearsOld$SEX    = factor(bearsOld$SEX)
+bearsOld$MONTH  = factor(bearsOld$MONTH)
+
+weightmod_5 = lm(log(WEIGHT)~log(LENGTH)+log(CHEST)+log(NECK)+log(HEADWTH)+
+                 log(HEADLEN)+SEX+MONTH+log(AGE), data=bearsOld)
+sink("g_output.txt")
+print(summary(weightmod_5))
+sink()
+
 
